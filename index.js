17ba10419552cf237db57639d019031fb22b2ca7
Danner Baumgartner <danner.baumgartner@gmail.com>
2023-08-23 14:05:41 -0700

    Passed all tests

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Your code here
+let dodger = document.getElementById("dodger")
+const moveDodgerRight = () =>{
+    dodger.style.left = `${parseInt(dodger.style.left.replace("px",""))+3}px`
+}
+const moveDodgerLeft = () =>{
+    dodger.style.left = `${parseInt(dodger.style.left.replace("px",""))-3}px`
+}
+const handler = (k) =>{
+    if(k.key=="ArrowRight"){
+        moveDodgerRight()
+    }
+    if(k.key=="ArrowLeft") {
+        moveDodgerLeft()
+    }
+}
+document.addEventListener("keydown",handler)
